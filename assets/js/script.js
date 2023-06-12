db3056891acaaa0c41634dedd3d45d76c4cba836
Daniel Prisco <82135697+dlprisco@users.noreply.github.com>
2023-06-12 13:45:27 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
 const nameInput = document.querySelector("input");
-
-const APIID = ${{ process.env.token }};
-
+/* const APIID = ${{ process.env.token }}; */
 var data = {}
 var lon = undefined;
 var lat = undefined;
-
 var current = {};
 
 function search_country() {
-	//window.onload(current_weather_this())
-	const city_name = document.querySelector("input").value;
+    const city_name = document.querySelector("input").value;
 	
-    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${APIID}`)
+    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=5a706f4605511ea6ad2c06c7a71d60e2`)
 	.then(function (response) {
 		data = response.data[0];
 		document.getElementById("main").innerHTML = '';
@@ -31,7 +27,6 @@ function search_country() {
 				let t = document.createElement('p');
 				t.innerHTML = `${key}: ${data[key]}`;
 				document.getElementById("main").appendChild(t);		
-				//loadTableData(, "country");
 			}
 		}
 		
@@ -50,16 +45,11 @@ function search_country() {
 	})	
 }
 
-function display_info() {
-	document.getElementById("app").innerHTML = '';
-	//axios.get(``)
-}
-
-  function current_weather() {
-	  let t = document.createElement('h2');
-	  t.innerHTML = 'Current Weather'
-	  document.getElementById("main").appendChild(t)
-  	axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIID}&units=metric`)
+function current_weather() {
+    let t = document.createElement('h2');
+    t.innerHTML = 'Current Weather'
+    document.getElementById("main").appendChild(t)
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIID}&units=metric`)
 	.then(function (response) {
 		current = response.data;
 		for (const key in current) {
@@ -111,68 +101,3 @@ function display_info() {
 		console.log(error);
 	})	
   }
-
-/*
-  function fib(n, cnt) {
-	  if (cnt <= 2) {
-		  
-	  }
-	  
-  }
-  
- 
- function current_weather_this() {
-  	() => {
-	.then(function (response) {
-		current = {'main':2, 'list':{'item':1, 'item1':2}, 'numbers': 2, 'array':[{'l':2}, 0};
-		for (const key in current) {		
-			if (typeof(current[ley]) === 'object') {
-				let n_l = document.createElement('ul')
-				
-				for (const nested_key in key) {
-					let n_li = document.createElement("li")
-					n_li.innerHTML = `${nested_key} ${key[nested_key]}`
-					n_l.appendChild(n_li)
-				}
-				document.getElementById("main").appendChild(nl);
-			}
-			else {
-				let t = document.createElement('p');
-				t.innerHTML = key;
-				document.getElementById("main").appendChild(t);		
-			}
-		}
-		console.log(current);
-	})
-	.catch(function (error) {
-		// handle error
-		console.log(error);
-	})	
-  }
-  
-   function foo() {
-		current = {'main':2, 'list':{'item':1, 'item1':2}, 'numbers': 2, 'array':[{'l':2}, 0]};
-		for (const key in current) {		
-			if (typeof(current[key]) === 'object') {
-				let n_l = document.createElement('ul')
-				
-				for (const nested_key in key) {
-					let n_li = document.createElement("li")
-					n_li.innerHTML = `${nested_key} ${key[nested_key]}`
-					n_l.appendChild(n_li)
-				}
-				document.getElementById("main").appendChild(n_l);
-			}
-			else {
-				let t = document.createElement('p');
-				t.innerHTML = key;
-				document.getElementById("main").appendChild(t);		
-			}
-		}
-   }
- function footer_display() {
-	 let footer = document.getElementById("foo");
-	 footer.innerHTML ='<h1>hello world</h1>';
- }
- */
- 
