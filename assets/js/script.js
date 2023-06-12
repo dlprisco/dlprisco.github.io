c576a8ca2678e80bd56afce08252eb6a5ca0d757
Daniel Prisco <82135697+dlprisco@users.noreply.github.com>
2023-06-12 13:53:00 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var current = {};
 function search_country() {
     const city_name = document.querySelector("input").value;
 	
-    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=5a706f4605511ea6ad2c06c7a71d60e2`)
+    axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=5a706f4605511ea6ad2c06c7a71d60e2`)
 	.then(function (response) {
 		data = response.data[0];
 		document.getElementById("main").innerHTML = '';
@@ -49,7 +49,7 @@ function current_weather() {
     let t = document.createElement('h2');
     t.innerHTML = 'Current Weather'
     document.getElementById("main").appendChild(t)
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIID}&units=metric`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIID}&units=metric`)
 	.then(function (response) {
 		current = response.data;
 		for (const key in current) {
